78e88dec30d835f46bdd2344a3cc6e52158b8895
Matt Kollross <kollross@illinois.edu>
2026-03-05 09:30:56 -0600

    Coooommmmiiiitttt

--- .zsh/aliases_macos.zsh
+++ .zsh/aliases_macos.zsh
@@ -1,11 +1,11 @@
-alias ls="gls --color=auto"
-alias ll="gls --color=auto -alG"
+alias ls="eza"
+alias ll="eza -al"
+alias lr='eza -al --sort=created'
 alias r='tmux attach -d'
 alias cp='cp -i'
 alias mv='mv -i'
 alias cl='clear'
 alias cls='clear'
-alias lr='ls -altr'
 alias h='ssh kollross@shaolin.kollross.net'
 alias jig='ssh jig.ncsa.illinois.edu'
 alias j='ssh sg1.ncsa.illinois.edu'
