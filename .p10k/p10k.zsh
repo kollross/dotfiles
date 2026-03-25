e5d2336b17dc2af9743796c828fcc23bc0bc8a1c
Matt Kollross <kollross@illinois.edu>
2026-03-25 09:18:42 -0500

    Coooommmmiiiitttt

--- .p10k/p10k.zsh
+++ .p10k/p10k.zsh
@@ -979,7 +979,7 @@
 
   ##################################[ context: user@hostname ]##################################
   # Context color when running with privileges.
-  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=196
+  typeset -g POWERLEVEL9K_CONTEXT_ROOT_FOREGROUND=197
   typeset -g POWERLEVEL9K_CONTEXT_ROOT_BACKGROUND=0
   # Context color in SSH without privileges.
   typeset -g POWERLEVEL9K_CONTEXT_{REMOTE,REMOTE_SUDO}_FOREGROUND=050
